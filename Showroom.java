97108ddfb49253725baac02741ef88d064d79c4f
Me-2502 <22bce257@nirmauni.ac.in>
2025-07-27 12:29:01 +0530

    Update Showroom.java

--- Showroom.java
+++ Showroom.java
@@ -1,41 +1,3 @@
-/*
-import java.sql.SQLOutput;
-import java.util.Scanner;
-public class Showroom implements utility{
-    String showroom_name;
-    String showroom_address;
-    int total_employees;
-    int total_cars_in_stock = 0;
-    String manager_name;
-
-    @Override
-    public void get_details() {
-        System.out.println("Showroom Name: "+showroom_name);
-        System.out.println("Showroom Address: "+showroom_address);
-        System.out.println("Manager Name: "+manager_name);
-        System.out.println("Total Employees: "+total_employees);
-        System.out.println("Total Cars In Stock: "+total_cars_in_stock);
-
-    }
-    @Override
-    public void set_details(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("======================= *** ENTER SHOWROOM DETAILS *** =======================");
-        System.out.println();
-        System.out.print("SHOWROOM NAME: ");
-        showroom_name = sc.nextLine();
-        System.out.print(("SHOWROOM ADDRESS: "));
-        showroom_address = sc.nextLine();
-        System.out.print("MANAGER NAME: ");
-        manager_name = sc.nextLine();
-        System.out.print("TOTAL NO OF EMPLOYEES: ");
-        total_employees = sc.nextInt();
-        System.out.print("TOTAL CARS IN STOCK: ");
-        total_cars_in_stock = sc.nextInt();
-
-    }
-}
-*/
 import java.util.Scanner;
 
 public class Showroom implements utility {  //utility is an interface 
@@ -56,7 +18,7 @@ public class Showroom implements utility {  //utility is an interface
 
     @Override
     public void set_details() {
-        try (Scanner sc = new Scanner(System.in)) {
+        try(Scanner sc = new Scanner(System.in)) {
             System.out.println("======================= *** ENTER SHOWROOM DETAILS *** =======================");
             System.out.println();
             System.out.print("SHOWROOM NAME: ");
@@ -67,8 +29,15 @@ public class Showroom implements utility {  //utility is an interface
             manager_name = sc.nextLine();
             System.out.print("TOTAL NO OF EMPLOYEES: ");
             total_employees = sc.nextInt();
+            if(total_employees < 0)
+                throw new IllegalArgumentException("Total employees cannot be negative.");
             System.out.print("TOTAL CARS IN STOCK: ");
             total_cars_in_stock = sc.nextInt();
+            if(total_cars_in_stock < 0)
+                throw new IllegalArgumentException("Total cars in stock cannot be negative.");
+        }
+        catch(Exception e) {
+            System.out.println("An error occurred while setting showroom details: " + e.getMessage());
         }
     }
 }
